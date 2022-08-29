c47321370fa755fe6f75ae3ad695f7e566a79d9c
Elnar Kilmukhametov <soulmiker@gmail.com>
2022-08-29 16:35:30 +0300

    fix #7

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -30,8 +30,8 @@ public class PlayerTest {
     @Test
     public void shouldThrowRunTimeException() {
         GameStore store = new GameStore();
-        Game game = store.publishGame("Call of Duty", "Battle Royale");
-        Game game1 = store.publishGame("Uncharted", "Battle Royale" );
+        Game game = store.publishGame("Call of Duty", "Battle Royal");
+        Game game1 = store.publishGame("Uncharted", "Battle Royal" );
         Game game3 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
         Player player = new Player("Petya");
@@ -42,5 +42,24 @@ public class PlayerTest {
             player.play(game3, 1);
 
         });
+    }
+    @Test
+    public void shouldMostPlayerByGenre() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Call of Duty", "Battle Royal");
+        Game game1 = store.publishGame("Uncharted", "Battle Royal");
+        Game game3 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+        player.installGame(game3);
+        player.play(game, 3);
+        player.play(game1, 2);
+        player.play(game3, 1);
+
+        String expected = "Call of Duty";
+        String actual = player.mostPlayerByGenre("Battle Royal");
+        assertEquals(expected, actual);
     }
 }
