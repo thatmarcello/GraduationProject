3f8e15a93c723fd4700ad27a5301ee9524ee1e5a
Mark Iarantsev <markyarantsev@yahoo.com>
2022-08-29 16:23:01 +0300

    add test for mostPlayerByGenre

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -38,19 +38,23 @@ public class PlayerTest {
     }
 
     @Test
-    public void addPlayGameNegativeValue() {
+    public void shouldMostPlayerByGenre() {
         GameStore store = new GameStore();
-        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game = store.publishGame("Call of Duty", "Battle Royale");
         Game game1 = store.publishGame("Uncharted", "Battle Royale");
+        Game game3 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
         Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+        player.installGame(game3);
+        player.play(game, 3);
+        player.play(game1, 2);
+        player.play(game3, 1);
 
-
-
-        assertThrows(Exception.class, () -> {
-            player.play(game1, -1);
-
-        });
+        String expected = "Call of Duty";
+        String actual = String.valueOf(player.mostPlayerByGenre("Battle Royale"));
+        assertEquals(expected, actual);
     }
 
 }
